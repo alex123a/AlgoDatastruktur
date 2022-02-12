c7818cae804fa1a01b989a11d3b95c7313ac74c0
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2022-02-12 10:01:09 +0100

    d

---
+++ Lektion2-3/insertionsort.py
@@ -0,0 +1,12 @@
+A = [3, 2, 1, 5, 6, 4]
+
+for j in range(1, len(A)):
+    key = A[j]
+    i = j - 1
+    while i >= 0 and A[i] > key:
+        A[i + 1] = A[i]
+        i = i - 1
+    A[i + 1] = key
+
+print(A)
+
