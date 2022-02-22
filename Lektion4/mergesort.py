848214690466db57bab3b0c30b1fa795271be664
Alex Valentiner <alex.valentiner@hotmail.com>
2022-02-22 23:28:19 +0100

    3

--- Lektion4/mergesort.py
+++ Lektion4/mergesort.py
@@ -1,5 +1,15 @@
+import time
+import random
+import math
 
-A = [3, 5, 2, 3, 1, 0, 13, 2]
+A = []
+size = 125000
+
+def fillArray():
+	A.clear()
+	for i in range(0, size + 1):
+		number = random.randint(1, 300)
+		A.append(number)
 
 def merge(A, p, q, r):
 	n1 = q - p + 1
@@ -19,7 +29,7 @@ def merge(A, p, q, r):
 	for k in range(p, r + 1):
 		# Task 4 start
 		if i < j and L[i] > R[j]:
-			print(count)
+			# print(count)
 			count += 1
 		# Task 4 end
 		if L[i] <= R[j]:
@@ -28,8 +38,6 @@ def merge(A, p, q, r):
 		else:
 			A[k] = R[j]
 			j = j + 1
-
-	
 	return A
 
 def mergeSort(A, p, r):
@@ -38,6 +46,25 @@ def mergeSort(A, p, r):
 		mergeSort(A, p, q)
 		mergeSort(A, q + 1, r)
 		merge(A, p, q, r)
-print(A)
+
+fillArray()
+startTime1 = time.time()
 mergeSort(A, 0, len(A) - 1)
-print(A)
+endTime1 = time.time()
+time1 = endTime1 - startTime1
+
+fillArray()
+startTime2 = time.time()
+mergeSort(A, 0, len(A) - 1)
+endTime2 = time.time()
+time2 = endTime2 - startTime2
+
+fillArray()
+startTime3 = time.time()
+mergeSort(A, 0, len(A) - 1)
+endTime3 = time.time()
+time3 = endTime3 - startTime3
+
+average = ((time1 + time2 + time3) / 3) * 1000
+divided = average / (size * math.log(size, 2))
+print(f'Time1: {time1}, time2: {time2}, time3: {time3} seconds\nAverage: {average} miliseconds\naverage / n * log n: {divided}')
